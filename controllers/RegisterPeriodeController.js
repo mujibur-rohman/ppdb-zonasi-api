942a96ffa6e24137d81a02d92d067929fff640f6
Mujiburrohman <muji.official06@gmail.com>
2023-04-04 21:04:28 +0700

    register periode

--- controllers/RegisterPeriodeController.js
+++ controllers/RegisterPeriodeController.js
@@ -5,6 +5,16 @@ import RegisterPeriod from "../models/RegisterPeriodeModel.js";
 export const createRegisterPeriode = async (req, res) => {
   try {
     const { tahunAjaran, startDate, kuota, endDate } = req.body;
+    // cek apakah tahun ajaran sudah tersedia
+    const foundAvailable = await RegisterPeriod.findOne({
+      where: {
+        tahunAjaran,
+      },
+    });
+
+    if (foundAvailable)
+      return res.status(406).json({ message: "Tahun ajaran sudah tersedia" });
+
     // buat register periode
     const registerPer = await RegisterPeriod.create({
       userId: req.id,
@@ -49,6 +59,50 @@ export const getRegisterPeriode = async (req, res) => {
       });
     });
     res.status(200).json(registerPeriod);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const updateRegisterPeriode = async (req, res) => {
+  try {
+    const { tahunAjaran, kuota, startDate, endDate } = req.body;
+    const { id } = req.params;
+    const registerPer = await RegisterPeriod.findOne({
+      where: {
+        id: id,
+      },
+    });
+    if (!registerPer)
+      return res.status(404).json({ message: "Register Periode Not Found" });
+
+    await RegisterPeriod.update(
+      {
+        userId: registerPer.userId,
+        tahunAjaran,
+        startDate,
+        endDate,
+      },
+      { where: { id: registerPer.id } }
+    );
+
+    // looping jurusan
+    kuota.forEach(async (item) => {
+      await Kuota.update(
+        {
+          registerPeriodId: registerPer.id,
+          jurusanId: item.jurusan,
+          kuota: item.kuota,
+        },
+        {
+          where: {
+            registerPeriodId: registerPer.id,
+          },
+        }
+      );
+    });
+
+    res.status(200).json({ message: "Register Periode Upddated!" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
--- routers/RegisterPeriodRoute.js
+++ routers/RegisterPeriodRoute.js
@@ -2,12 +2,25 @@ import express from "express";
 import {
   createRegisterPeriode,
   getRegisterPeriode,
+  updateRegisterPeriode,
 } from "../controllers/RegisterPeriodeController.js";
+import { body } from "express-validator";
 import verifyJWT from "../controllers/verifyJWT.js";
+import { checkErrorsBody } from "../middleware/checkErrorsBody.js";
 
 const RegisterPeriodRoute = express.Router();
 
 RegisterPeriodRoute.post("/register-periode", verifyJWT, createRegisterPeriode);
-RegisterPeriodRoute.get("/register-periode", getRegisterPeriode);
+RegisterPeriodRoute.get("/register-periode", verifyJWT, getRegisterPeriode);
+RegisterPeriodRoute.put(
+  "/register-periode/:id",
+  body("tahunAjaran").notEmpty(),
+  body("startDate").notEmpty(),
+  body("endDate").notEmpty(),
+  body("kuota").notEmpty(),
+  checkErrorsBody,
+  verifyJWT,
+  updateRegisterPeriode
+);
 
 export default RegisterPeriodRoute;
